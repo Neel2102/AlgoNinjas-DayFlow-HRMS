8c076cd76dfb107f9748a0c4355365512ef67ea0
Dhrumil <dhumil05@gmail.com>
2026-01-03 09:07:55 +0530

    navbar.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LandingPage from './pages/landingpage.jsx'
+import LandingPage from './pages/landingpage'
+import Navbar from './pages/navbar'
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />   {/* Header */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
       </Routes>
